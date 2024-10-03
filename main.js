695f8913e5e53f77422e1c18f116b692931c9dd6
nikosban <nbanis30@gmail.com>
2024-10-03 20:26:07 +0200

    Added the initial logic

--- main.js
+++ main.js
@@ -1,41 +1,115 @@
-  let numberA;
-  let numberB;
-  let operator;
-  let result;
+// initialise global variables
+let numberA;
+let numberB;
+let operator;
+let result;
+let numberArray = [];
 
-const number = document.getElementById("numberBtn");
+// initialise the app
 
-  number.addEventListener("click", () => {
-      numberB = parseFloat(number.value)
-      numberA = parseFloat(number.value)
-      console.log(numberA);
-      console.log(numberB);
-  });
+function initCalc () {
 
-const operatorBtn = Array.from(document.getElementsByClassName("operator"));
-  
- operatorBtn.forEach((button) => {
-    button.addEventListener("click", () => {
-         operator = button.value;
-         console.log(operator);
+  // add event listners to the number buttons
+
+   const numberBtn = Array.from(document.getElementsByClassName("numberBtn"));
+
+   numberBtn.forEach((number) => {
+    number.addEventListener("click", () => {
+      getNumber(number);
+    } )
+   })
+
+  // add event listeners to the operator buttons
+
+  const operBtn = Array.from(document.getElementsByClassName("operator"));
+
+  operBtn.forEach((operation) => {
+    operation.addEventListener("click", () => {
+      getOperator(operation);
     })
- });
+  })
 
- const calc = document.getElementById("operate");
+  // initialise the result with an empty string
 
- calc.addEventListener("click", () => {
-    operate();
- })
+  const resultsArea = document.getElementById("resultsArea");
+
+  resultsArea.textContent = '0';
+
+  const resultBtn = document.getElementById("operate");
+
+  resultBtn.addEventListener("click", () => {
+    runCalculation();
+  })
+}
+
+function getNumber(number) {
+   if (!operator) {
+    getNumberA(number);
+   }
+
+   else {
+    getNumberB(number);
+   }
+}
 
 
- function operate () {
-    if (operator === "+") {
+//get the first number
+
+function getNumberA (number) {
+  numberArray.push(number.value);
+  numberA = numberArray.join('');
+  numberA = parseFloat(numberA);
+  resultsArea.textContent = numberA;
+}
+
+//get the operator
+
+function getOperator(operation) {
+  operator = operation.value;
+  resultsArea.textContent = numberA + operator;
+  numberArray = [];
+}
+
+//get the second number
+
+function getNumberB(number) {
+  numberArray.push(number.value);
+  numberB = numberArray.join('');
+  numberB = parseFloat(numberB);
+  resultsArea.textContent = numberA + operator + numberB;
+}
+
+//run the calculation
+
+function runCalculation() {
+  if (!numberA) {
+    getNumber(number);
+    resultsArea.textContent = "Select a number";
+  }
+
+  else {
+    switch (operator) {
+      case "+":
         result = numberA + numberB;
-    }
-
-    else if (operator = "/") {
+        break;
+      case "-":
+        result = numberA - numberB;
+        break;
+      case "/":
         result = numberA / numberB;
+        break;
+      case "*":
+        result = numberA * numberB;
+        break;
+      case "%":
+        result = numberA % numberB;
+        break;
     }
-    
-    console.log(result);
- }
+  }
+
+  resultsArea.textContent = result;
+}
+
+
+
+initCalc();
